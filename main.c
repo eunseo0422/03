4734e60038f348bd24b5eb43f89b812fa531ca4d
eunseo0422 <ukibest@naver.com>
2023-09-21 16:24:35 +0900

    3

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-int main(void) 
-{ 
-char c;
-int i;
 
-printf("input a number :");
+int main(void)
+{
+char c;
+
+printf("enter a character : ");
 scanf("%c", &c);
 
-i = c - '0';
-printf("The input number ASCII is %i\n",c);
-printf("The input number is %i\n", i);
+printf("the next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
 
 return 0;
 }
-
+ 

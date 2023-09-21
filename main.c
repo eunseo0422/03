66eede901dd3ba86bb9621cc049332db8dd268c8
eunseo0422 <ukibest@naver.com>
2023-09-21 16:11:00 +0900

    2

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
-	int input_int;
-	float input_float;
+int main(void) 
+{ 
+char c;
+int i;
 
-	printf("enter an integer : ");
-	scanf("%d", &input_int);
+printf("input a number :");
+scanf("%c", &c);
 
+i = c - '0';
+printf("The input number ASCII is %i\n",c);
+printf("The input number is %i\n", i);
 
-	printf("enter a float : ");
-	scanf("%f", &input_float);
-
-	printf("integer : %d, float : %f\n", input_int, input_float);
-
-	return 0;
+return 0;
 }
+
